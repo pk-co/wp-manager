432b726b8237a057692fe8962312eff62dcfb886
pk-co <prasacode@gmail.com>
2024-12-31 04:37:04 +0530

    [ADD] header requirements

---
+++ wp-managers.php
@@ -0,0 +1,14 @@
+<?php 
+
+    /* 
+    Plugin Name: WP Manager
+    Description: A Simple Plugin to manage Managers (Add, View, Edit, Delete)
+    Version: 1.0.1
+    Author: Prasa
+    Plugin URI: https://example.plugin
+    AUthor URI: https://example.prasa.com
+    */
+
+
+        
+?>
