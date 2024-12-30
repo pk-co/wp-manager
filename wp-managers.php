de7b2db8c4367587adbaa414272865bdd8e3b713
pk-co <prasacode@gmail.com>
2024-12-31 05:26:16 +0530

    [DEV] page view

--- wp-managers.php
+++ wp-managers.php
@@ -31,4 +31,42 @@ function wp_add_admin_menu(){
     );
 }
 
-?>
+// admin page
+function wp_admin_page(){
+    
+    
+    ?>
+     <div class="wrap">
+        <h1>Managers</h1>
+        <h2>Add Manager</h2>
+        <form>
+            <table>
+                <tr>
+                    <th>Name: </th>
+                    <td><input type="text" name="name" value="" required></td>
+                </tr>
+                <tr>
+                    <th>Age: </th>
+                    <td><input type="number" name="age" value="" required></td>
+                </tr>
+                <tr>
+                    <th>Phone: </th>
+                    <td><input type="text" name="phone" value="" required></td>
+                </tr>
+                <tr>
+                    <th>Email: </th>
+                    <td><input type="email" name="email" value="" required></td>
+                </tr>
+                <?php ?>
+
+            </table>
+            <p><input type="submit" name="add_manager" class="button-primary" value="Add Manager"></p>
+        </form>
+
+        <h2>Manager List</h2>
+     </div>
+
+    <?php
+    
+}
+
